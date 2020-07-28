b7aff52f2de64e01ce9b9db9382fb7075a8f5073
carlos.joaquim <carloshj2011@gmail.com>
2020-07-28 07:58:10 -0300

    Structure controller and service

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,33 @@
 package users
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/carloshjoaquim/bookstore-users-api/domain/users"
+	"github.com/carloshjoaquim/bookstore-users-api/services"
+	"github.com/carloshjoaquim/bookstore-users-api/utils/errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func GetUser(c *gin.Context) {
 
 }
 
 func CreateUser(c *gin.Context) {
+	var user users.User
 
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func SearchUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,11 @@
 package services
+
+import (
+	"github.com/carloshjoaquim/bookstore-users-api/domain/users"
+	"github.com/carloshjoaquim/bookstore-users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+
+	return &user, nil
+}
